8531dbe409d274c5e42d1c46b2f9c4cea8f90fa0
jaicychoi <jaicychoi@gamil.com>
2020-09-18 17:28:28 +0900

    animation and responsive code added

---
+++ js/main.js
@@ -0,0 +1,116 @@
+if(window.NodeList && !NodeList.prototype.forEach)
+    NodeList.prototype.forEach = Array.prototype.forEach;
+
+const nav_wrapper = document.querySelector('.nav_wrapper');
+const menu_mini_icon = document.querySelector('.menu_mini_icon');
+const menu_mini = document.querySelector('.menu_mini');
+const menu_mini_li = document.querySelectorAll('.menu_mini>li');
+
+let menu_click = false;
+
+menu_mini_icon.addEventListener('click', function(){
+    if( menu_click === false ){
+        menu_click = true;
+        menu_mini_icon.classList.add('show');
+        menu_mini.classList.add('show');
+        menu_mini_li.forEach(function(li){
+            li.classList.add('open');
+        });
+    }
+    else{
+        menu_click = false;
+        menu_mini_icon.classList.remove('show');
+        menu_mini.classList.remove('show');
+        menu_mini_li.forEach(function(li){
+            li.classList.remove('open');
+        });
+    }
+});
+
+const go_top = document.querySelector('.go_top');
+const conetent_description = document.querySelector('.conetent_description');
+const gallery1_div = document.querySelectorAll('.gallery1_wrap>div');
+const gallery2_div = document.querySelectorAll('.gallery2_wrap>div');
+const gruop2 = document.querySelector('.group2');
+const group2_text_box = document.querySelector('.group2_text_box');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const group3_text_box = document.querySelector('.group3_text_box');
+const wedding_img = document.querySelectorAll('.wedding_img');
+const wedding_text_box_wrap = document.querySelectorAll('.wedding_text_box_wrap');
+const group4 = document.querySelector('.group4');
+const group4_text_box = document.querySelector('.group4_text_box');
+const group5_logo = document.querySelector('.group5_logo');
+const group5_text_box = document.querySelector('.group5_text_box');
+const group5_title = document.querySelector('.group5_title');
+const group5_img_shader = document.querySelector('.group5_img_shader');
+
+window.addEventListener('scroll', function(){
+    //show move top btn
+    if( window.pageYOffset >= window.innerHeight/2 )
+        go_top.classList.add('show');
+    else
+        go_top.classList.remove('show');
+
+    //show group1 text
+    if( window.pageYOffset >= 130 )
+        conetent_description.classList.add('show');
+
+    //fix navigator and show gallery images
+    if( window.pageYOffset >= 200){
+        nav_wrapper.classList.add('scroll');
+        gallery1_div.forEach(function(div){
+            div.classList.add('show');
+        });
+        gallery2_div.forEach(function(div){
+            div.classList.add('show');
+        });
+    }
+    else nav_wrapper.classList.remove('scroll');
+
+    //show group2 texts
+    if( window.pageYOffset >= 1500 ){
+        gruop2.classList.add('show');
+        group2_text_box.classList.add('show');
+        left.classList.add('show');
+        right.classList.add('show');
+    }
+
+    //show group3 text
+    if( window.pageYOffset >= 2200 )
+        group3_text_box.classList.add('show');
+    //show group3 wedding images and texts
+    if( window.pageYOffset >= 2600 ){
+        wedding_img[0].classList.add('show');
+        wedding_text_box_wrap[0].classList.add('show');
+    }
+    if( window.pageYOffset >= 2800 ){
+        wedding_img[1].classList.add('show');
+        wedding_text_box_wrap[1].classList.add('show');
+    }
+    if( window.pageYOffset >= 3000 ){
+        wedding_img[2].classList.add('show');
+        wedding_text_box_wrap[2].classList.add('show');
+    }
+
+    //show group4
+    if( window.pageYOffset >= 4000 ){
+        group4.classList.add('show');
+        group4_text_box.classList.add('show');
+    }
+
+    //show group5 logo amd text
+    if( window.pageYOffset >= 4800 ){
+        group5_logo.classList.add('scale');
+        group5_text_box.classList.add('show');
+        group5_title.classList.add('show');
+    }
+    //show group5 image
+    if( window.pageYOffset >= 5500 )
+        group5_img_shader.classList.add('whip');
+});
+go_top.addEventListener('click', function(){
+    document.querySelector('header').scrollIntoView({behavior:'smooth'});
+});
+
+//Add footer animation
